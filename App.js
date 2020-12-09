636b5e2e6f79b89a58a3957e6cbc7d8df31ca41e
getfolio-tamplates <74338109+getfolio-tamplates@users.noreply.github.com>
2020-12-09 18:33:48 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello.js';
+
+function App() {
+  return (
+    <div className="App">
+ <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css'
+
+
+
+const Hello=()=>{
+  return <h1>Hello</h1>
+}
+export default Hello
